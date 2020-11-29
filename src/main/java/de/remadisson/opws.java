67960894a66b56f047bfcd7173b60fee55663f2e
Maximilian Loers <maximilianloers@icloud.com>
2020-11-29 18:34:26 +0100

    init

---
+++ src/main/java/de/remadisson/opws.java
@@ -0,0 +1,17 @@
+package de.remadisson;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class opws extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
